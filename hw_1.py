960dcaebc1e58496a1fa6ed14afa734d79301a97
Kerally <110418322+Kerally@users.noreply.github.com>
2022-08-15 21:22:12 +0300

    Create hw_1.py

---
+++ hw_1.py
@@ -0,0 +1,10 @@
+# import this - так званий Дзен Python. Філосовські думки та настанови для користувача python
+import this 
+# import antigravity - інтернет комікс про сарказм, математику і python в цілому
+import antigravity
+# import __hello__ - виводить в термінал вітання зі світом 
+import __hello__
+# from __future__ import braces - виводить "Not a chance" - без шансів - відповідь на запит людей про додавання дужок в Python
+from __future__ import braces
+
+
